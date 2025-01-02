ad5d92f64f9ca2ec478976bfd8db9762cfe5a282
Zack <168221270+Zval321@users.noreply.github.com>
2025-01-02 13:51:05 -0600

    added several items to the menu using seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -104,3 +104,57 @@
 # else 
 #   puts "Failed to create category: #{category.errors_full_messages.join(', ')}"
 # end 
+
+food = Food.new(
+  name: "Spicy Tuna Roll",
+  price: 7.50,
+  image_url: 'https://www.alyonascooking.com/wp-content/uploads/2015/10/spicy-tuna-roll-9.jpg',
+  description: 'Fresh tuna, spicy mayo',
+  category_id: 1
+)
+if food.save 
+  puts "Food created successfully: #{food.name}"
+else 
+  puts "Failed to create food: #{food.errors_full_messages.join(', ')}"
+end 
+
+food = Food.new(
+  name: "Spicy Salmon Roll",
+  price: 6.99,
+  image_url: 'https://simplyhomecooked.com/wp-content/uploads/2022/06/spicy-salmon-roll-4.jpg',
+  description: 'Fresh salmon, spicy mayo',
+  category_id: 1
+)
+if food.save 
+  puts "Food created successfully: #{food.name}"
+else 
+  puts "Failed to create food: #{food.errors_full_messages.join(', ')}"
+end 
+
+food = Food.new(
+  name: "Salmon Roll",
+  price: 6.50,
+  image_url: 'https://popmenucloud.com/cdn-cgi/image/width%3D1200%2Cheight%3D1200%2Cfit%3Dscale-down%2Cformat%3Dauto%2Cquality%3D60/cswnyzje/09b14a68-c399-4b23-ab15-b2f622e78ae6.jpg',
+  description: 'Fresh salmon',
+  category_id: 1
+)
+if food.save 
+  puts "Food created successfully: #{food.name}"
+else 
+  puts "Failed to create food: #{food.errors_full_messages.join(', ')}"
+end 
+
+food = Food.new(
+  name: "Tuna Roll",
+  price: 6.99,
+  image_url: 'https://takestwoeggs.com/wp-content/uploads/2023/08/Tekka-Maki-Tuna-Sushi-Roll-Takestwoeggs-sq.jpg',
+  description: 'Fresh tuna',
+  category_id: 1
+)
+if food.save 
+  puts "Food created successfully: #{food.name}"
+else 
+  puts "Failed to create food: #{food.errors_full_messages.join(', ')}"
+end 
+
+
