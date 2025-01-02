dacb67d984b420a89c1100e70a251da6fd4a6a2e
Zack <168221270+Zval321@users.noreply.github.com>
2025-01-02 13:38:30 -0600

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173'  # Replace with your front-end origin if different
-    
+    # Replace with your front-end origins
+    origins 'http://localhost:5173', 'https://zen-rolls-frontend-7kejarejv-zackvalavanis-projects.vercel.app'
+
+    # Allow all resources with the following settings
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
