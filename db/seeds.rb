9398feb0fcf0021c06eb533d16905b885feb1466
Zack <168221270+Zval321@users.noreply.github.com>
2025-01-02 14:05:11 -0600

    added menu items

--- db/seeds.rb
+++ db/seeds.rb
@@ -105,12 +105,64 @@
 #   puts "Failed to create category: #{category.errors_full_messages.join(', ')}"
 # end 
 
+# food = Food.new(
+#   name: "Spicy Tuna Roll",
+#   price: 7.50,
+#   image_url: 'https://www.alyonascooking.com/wp-content/uploads/2015/10/spicy-tuna-roll-9.jpg',
+#   description: 'Fresh tuna, spicy mayo',
+#   category_id: 1
+# )
+# if food.save 
+#   puts "Food created successfully: #{food.name}"
+# else 
+#   puts "Failed to create food: #{food.errors_full_messages.join(', ')}"
+# end 
+
+# food = Food.new(
+#   name: "Spicy Salmon Roll",
+#   price: 6.99,
+#   image_url: 'https://simplyhomecooked.com/wp-content/uploads/2022/06/spicy-salmon-roll-4.jpg',
+#   description: 'Fresh salmon, spicy mayo',
+#   category_id: 1
+# )
+# if food.save 
+#   puts "Food created successfully: #{food.name}"
+# else 
+#   puts "Failed to create food: #{food.errors_full_messages.join(', ')}"
+# end 
+
+# food = Food.new(
+#   name: "Salmon Roll",
+#   price: 6.50,
+#   image_url: 'https://popmenucloud.com/cdn-cgi/image/width%3D1200%2Cheight%3D1200%2Cfit%3Dscale-down%2Cformat%3Dauto%2Cquality%3D60/cswnyzje/09b14a68-c399-4b23-ab15-b2f622e78ae6.jpg',
+#   description: 'Fresh salmon',
+#   category_id: 1
+# )
+# if food.save 
+#   puts "Food created successfully: #{food.name}"
+# else 
+#   puts "Failed to create food: #{food.errors_full_messages.join(', ')}"
+# end 
+
+# food = Food.new(
+#   name: "Tuna Roll",
+#   price: 6.99,
+#   image_url: 'https://takestwoeggs.com/wp-content/uploads/2023/08/Tekka-Maki-Tuna-Sushi-Roll-Takestwoeggs-sq.jpg',
+#   description: 'Fresh tuna',
+#   category_id: 1
+# )
+# if food.save 
+#   puts "Food created successfully: #{food.name}"
+# else 
+#   puts "Failed to create food: #{food.errors_full_messages.join(', ')}"
+# end 
+
 food = Food.new(
-  name: "Spicy Tuna Roll",
-  price: 7.50,
-  image_url: 'https://www.alyonascooking.com/wp-content/uploads/2015/10/spicy-tuna-roll-9.jpg',
-  description: 'Fresh tuna, spicy mayo',
-  category_id: 1
+  name: "Chicago Roll",
+  price: 10.50,
+  image_url: 'https://136592157.cdn6.editmysite.com/uploads/1/3/6/5/136592157/s644165306373586183_p17_i1_w5472.jpeg?width=2560&dpr=2',
+  description: 'Tuna, albacore tuna, scallions, spicy mayo',
+  category_id: 2
 )
 if food.save 
   puts "Food created successfully: #{food.name}"
@@ -119,11 +171,11 @@ else
 end 
 
 food = Food.new(
-  name: "Spicy Salmon Roll",
-  price: 6.99,
-  image_url: 'https://simplyhomecooked.com/wp-content/uploads/2022/06/spicy-salmon-roll-4.jpg',
-  description: 'Fresh salmon, spicy mayo',
-  category_id: 1
+  name: "Samurai Roll",
+  price: 12.99,
+  image_url: 'https://136592157.cdn6.editmysite.com/uploads/1/3/6/5/136592157/s644165306373586183_p94_i1_w5472.jpeg?width=2560&dpr=2',
+  description: 'Salmon, avocado, cucumber, chili mayo, wrapped with soy paper, with tempura crunch, eel sauce, and wasabi mayo on top',
+  category_id: 2
 )
 if food.save 
   puts "Food created successfully: #{food.name}"
@@ -132,11 +184,11 @@ else
 end 
 
 food = Food.new(
-  name: "Salmon Roll",
-  price: 6.50,
-  image_url: 'https://popmenucloud.com/cdn-cgi/image/width%3D1200%2Cheight%3D1200%2Cfit%3Dscale-down%2Cformat%3Dauto%2Cquality%3D60/cswnyzje/09b14a68-c399-4b23-ab15-b2f622e78ae6.jpg',
-  description: 'Fresh salmon',
-  category_id: 1
+  name: "Dragon Roll",
+  price: 14.99,
+  image_url: 'https://136592157.cdn6.editmysite.com/uploads/1/3/6/5/136592157/s644165306373586183_p304_i2_w5472.jpeg?width=2560&dpr=2',
+  description: 'Eel and smelt roe, with eel, eel sauce and avocado on top',
+  category_id: 2
 )
 if food.save 
   puts "Food created successfully: #{food.name}"
@@ -145,11 +197,24 @@ else
 end 
 
 food = Food.new(
-  name: "Tuna Roll",
-  price: 6.99,
-  image_url: 'https://takestwoeggs.com/wp-content/uploads/2023/08/Tekka-Maki-Tuna-Sushi-Roll-Takestwoeggs-sq.jpg',
-  description: 'Fresh tuna',
-  category_id: 1
+  name: "Rainbow Roll",
+  price: 14.99,
+  image_url: 'https://136592157.cdn6.editmysite.com/uploads/1/3/6/5/136592157/s644165306373586183_p295_i1_w1735.jpeg?dpr=2',
+  description: 'Imitation crab, avocado with tuna, salmon, yellowtail and shrimp on top',
+  category_id: 2
+)
+if food.save 
+  puts "Food created successfully: #{food.name}"
+else 
+  puts "Failed to create food: #{food.errors_full_messages.join(', ')}"
+end 
+
+food = Food.new(
+  name: "Caterpillar Roll",
+  price: 15.99,
+  image_url: 'https://136592157.cdn6.editmysite.com/uploads/1/3/6/5/136592157/s644165306373586183_p336_i4_w3888.jpeg?width=2560&dpr=2',
+  description: 'Eel, spicy mayo, tempura crunch, wrapped with soy paper, with avocado and eel sauce on top',
+  category_id: 2
 )
 if food.save 
   puts "Food created successfully: #{food.name}"
