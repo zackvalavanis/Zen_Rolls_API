f3f1989f5133c37a9fb60076079d0944eba29a9a
Zack <168221270+Zval321@users.noreply.github.com>
2025-03-10 14:29:41 -0500

    updates

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', 'https://zen-rolls-frontend-hcej3ykuh-zackvalavanis-projects.vercel.app'
+    origins 'http://localhost:5173', 'https://zen-rolls-frontend-cwclm5qzl-zackvalavanis-projects.vercel.app'
 
     resource '*',
       headers: :any,
