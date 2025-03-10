9e53aff9b08cc8b64e145eeb4cbdd58603d774c5
Zack <168221270+Zval321@users.noreply.github.com>
2025-03-10 12:52:53 -0500

    Add debug log for secret_key_base

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,27 @@
 require_relative "boot"
-require "rails/all" # Ensure you have this line
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 module ZenRollsApi
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Set secret_key_base to read from the environment variable
-    config.secret_key_base = ENV['SECRET_KEY_BASE']
-
-    # Check if Rails.logger is available before trying to log
-    if Rails.logger
-      Rails.logger.debug "Secret Key Base: #{config.secret_key_base}"
-    end
-
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
