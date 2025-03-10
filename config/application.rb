1ad6fb59147ce14accd554dbb20dbf1648db090b
Zack <168221270+Zval321@users.noreply.github.com>
2025-03-10 12:46:15 -0500

    Add debug log for secret_key_base

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,10 @@ module ZenRollsApi
     # Set secret_key_base to read from the environment variable
     config.secret_key_base = ENV['SECRET_KEY_BASE']
 
-    # Add logging to check the secret_key_base
-    Rails.logger.debug "Secret Key Base: #{config.secret_key_base}"
+    # Check if Rails.logger is available before trying to log
+    if Rails.logger
+      Rails.logger.debug "Secret Key Base: #{config.secret_key_base}"
+    end
 
     config.autoload_lib(ignore: %w(assets tasks))
   end
