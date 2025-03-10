d077549b84f1e50e8fd2f5483d2a4ca2fcdc7b1c
Zack <168221270+Zval321@users.noreply.github.com>
2025-03-10 14:36:04 -0500

    updates

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,6 +6,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,  # Allow any headers
       methods: [:get, :post, :put, :patch, :delete, :options, :head],  # Allowed methods
       expose: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials'],  # Exposed headers
-      credentials: true  # Allow credentials (cookies, HTTP authentication)
+      credentials: false  # Allow credentials (cookies, HTTP authentication)
   end
 end
